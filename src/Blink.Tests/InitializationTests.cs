f3e49cdadf789376599663db5db14b6823c1e4da
Steve Cooper <steve@stevecooper.org>
2014-04-19 19:47:58 +0100

    empty projects

---
+++ src/Blink.Tests/InitializationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blink.Tests
+{
+    [TestClass]
+    public class InitializationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
