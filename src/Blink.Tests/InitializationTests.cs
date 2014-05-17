2de1b1b0faeefdab515f06c9dff694aec8ecaada
Steve Cooper <steve@stevecooper.org>
2014-05-17 23:40:54 +0100

    better tests

--- src/Blink.Tests/InitializationTests.cs
+++ src/Blink.Tests/InitializationTests.cs
@@ -51,18 +51,17 @@ namespace Blink.Tests
             // execute code, inside a transaction, with a fresh DB every time;
             factory.ExecuteDbCode(context =>
             {
-                // use the context here;
-                Assert.IsNotNull(context);
+                Assert.AreEqual(3, context.TestObjects.Count(), "should start with three");
 
                 context.TestObjects.Add(new TestObject { Name = "quux" });
                 context.SaveChanges();
-                Assert.AreEqual(4, context.TestObjects.Count(), "doesn't look transactional!");
+                Assert.AreEqual(4, context.TestObjects.Count(), "didn't save as expected");
 
                 called = true;
             }, context =>
             {
                 // make sure that the data persists into this next step;
-                Assert.AreEqual(4, context.TestObjects.Count(), "doesn't look transactional!");
+                Assert.AreEqual(4, context.TestObjects.Count(), "content was not persisted in chain!");
             });
 
             Assert.IsTrue(called);
@@ -70,7 +69,7 @@ namespace Blink.Tests
             factory.ExecuteDbCode(context =>
             {
                 // make sure that the data cidn't persist beyond the step sequence above;
-                Assert.AreEqual(3, context.TestObjects.Count(), "doesn't look transactional!");
+                Assert.AreEqual(3, context.TestObjects.Count(), "doesn't look transactional - we should have rolled back to three!");
             });
         }
 
